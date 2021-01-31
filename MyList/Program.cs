46aefb8c394e988c430880f6b2c1ab8174ee6d7f
Administrator <Administrator@DESKTOP-S1QF06L>
2021-01-31 20:16:17 +0300

    Güncelmendi

--- MyList/Program.cs
+++ MyList/Program.cs
@@ -14,36 +14,5 @@ namespace MyList
 
         }
     }
-    class BenimListem<T>
-    {
-        T[] _array;
-        T[] _tempArray;
-
-        public BenimListem()
-        {
-            _array = new T[0];
-        }
-        public void Add(T item)
-        {
-            _tempArray = _array;
-            _array = new T[_array.Length + 1];
-
-            for (int i = 0; i < _tempArray.Length; i++)
-            {
-                _array[i] = _tempArray[i];
-            }
-
-            _array[_array.Length - 1] = item;
-
-        }
-
-
-        public int Count
-        {
-            get { return _array.Length; }
-
-        }
-
-    }
 }
 
